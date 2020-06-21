c9e2af3643b4511ec2ee31bee8dd257abf9c84a9
jerp86 <jerp4@hotmail.com>
2020-06-21 20:11:46 -0300

    Add Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import CompareStates from '../pages/CompareStates';
+import SingleState from '../pages/SingleState';
+import Dashboard from '../pages/Dashboard';
 
 const Routes = () => (
   <Switch>
-    <Route path="/" exact component={CompareStates} />
+    <Route path="/" exact component={Dashboard} />
+    
+    <Route path="/compareDate" component={CompareStates} />
+    <Route path="/singleDate" component={SingleState} />
   </Switch>
 );
 
