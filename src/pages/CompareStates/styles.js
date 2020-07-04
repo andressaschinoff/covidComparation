2b7fe82ba5dcadeee0f1725cb5496eb037d06150
jerp86 <jerp4@hotmail.com>
2020-07-04 12:03:05 -0300

    Updated style in CompareStates page

--- src/pages/CompareStates/styles.js
+++ src/pages/CompareStates/styles.js
@@ -1,29 +1,61 @@
 import styled from 'styled-components';
-import { shade } from 'polished';
 
 export const Container = styled.section`
-  background-color: #61fcc3;
+  background-color: #28ecce;
+  grid-area: compareStates;
 `;
 
 export const Heading = styled.div`
-  
+  padding-top: 56px;
+  color: #003399;
+  font-size: 28px;
+  font-weight: bold;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 export const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100%;
   
+  flex-wrap: wrap;
+
+  .selects {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
 `;
 
 export const Select = styled.div`
-  
+  margin-top: 8px;
+
+  label {
+    font-size: 18px;
+  }
 `;
 
 export const CovidList = styled.div`
-  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h3 {
+    margin-top: 16px;
+    margin-bottom: 8px;
+    text-align: center;
+    font-size: 18px;
+    color: #003399;
+    font-weight: bold;
+  }
 `;
 
 export const CovidItem = styled.div`
-`;
-
-export const Calendar = styled.aside`
-  
+  margin: 0 16px;
 `;
